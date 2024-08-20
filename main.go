44c3eeb53447844c9c43adaa0a913cb869910f02
william-barros-costa <william.de.barros.da.costa@gmail.com>
2024-08-20 14:31:54 +0000

    Reads From Terminal

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-im
+package main
+
+import "fmt"
+import "os"
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Fprintln(os.Stdout, "There were no arguments provided")
+		os.Exit(0)
+	}
+	fmt.Println(os.Args)
+}
+
+/*
+Add Task
+List Tasks
+Remove Task
+Edit Task
+Mark completed
+List Completed tasks
+*/
