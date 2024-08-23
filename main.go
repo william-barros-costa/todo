c5414170bf6dfb2155ab9eebfbf9a24b4aac9826
william-barros-costa <william.de.barros.da.costa@gmail.com>
2024-08-23 10:25:40 +0000

    Added help message

--- main.go
+++ main.go
@@ -1,14 +1,38 @@
 package main
 
-import "fmt"
-import "os"
+import (
+	"fmt"
+	"os"
+)
+
+// Uses docopt format
+const help_message string = `CLI tool used to keep track of to do list
+
+Usage: todo COMMAND [OPTIONS]
+
+Commands:
+  add            Adds a task to the todo list
+  list           Lists tasks in the todo list
+  delete         Deletes a task from the todo list
+  edit           Edits a task
+  complete       Marks a task as completed 
+
+Global Options:
+  -h --help      Show this screen.
+  --version      Show version.
+
+Run 'docker COMMAND --help' for more information on a command.`
 
 func main() {
 	if len(os.Args) == 1 {
-		fmt.Fprintln(os.Stdout, "There were no arguments provided")
+		print_help()
 		os.Exit(0)
 	}
 	fmt.Println(os.Args)
+}
+
+func print_help() {
+	fmt.Fprintln(os.Stdout, []any{help_message}...)
 }
 
 /*
@@ -19,4 +43,3 @@ Edit Task
 Mark completed
 List Completed tasks
 */
-
